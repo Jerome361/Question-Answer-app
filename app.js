b7daf6a5bd0a70ebb446dec9c973c78123a29515
Tumwesigye <Tumwesigye>
2020-04-05 08:43:45 +0300

    Intalled and configured mongoose

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 const jsonParser = require('body-parser').json;
 const routes = require('./routes/questoRoutes');
 const logger = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -14,6 +15,23 @@ const port = process.env.PORT || 4000;
 //register our middleware
 app.use(logger('dev'))
 app.use(jsonParser());
+
+//Database connection
+mongoose.connect('mongodb://localhost:27017/QnAnswer');
+
+//Monitor the status of the connection
+const db = mongoose.connection
+
+// Catch error event on connection
+db.on('error', err => {
+    console.error('connection error', err);
+  });
+
+// Open connection event
+  db.once('open', () => {
+    console.log('db connection successful');
+  
+  });
 
 //route handler middleware
 app.use('/question', routes);
