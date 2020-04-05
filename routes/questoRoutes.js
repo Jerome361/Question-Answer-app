a0da2103c0825a55ea8646d80049753dbce83088
Tumwesigye <Tumwesigye>
2020-04-05 12:33:35 +0300

    Implemented read questions route and create questions route

--- routes/questoRoutes.js
+++ routes/questoRoutes.js
@@ -2,90 +2,97 @@
 
 const express = require('express');
 const router = express.Router();
+const Question = require('../models').Question;
 
 //Read questions
-router.get('/', (req, res) => {
-    res.json({ response: 'I have a GET request' });
-  });
-  
-  //create questions
-  router.post('/', (req, res) => {
-    res.json({
-      response: 'I have a POST request /Question',
-      body: req.body.color
+router.get('/', (req, res, next) => {
+  Question.find({})
+    .sort({ createdAt: -1 })
+    .exec(function (err, questions) {
+      if (err) return next(err);
+      res.json(questions);
     });
+});
+
+//create questions
+router.post('/', (req, res, next) => {
+  let question = new Question(req.body);
+  question.save((err, question) => {
+    if (err) return next(err);
+    res.status(201);
+    res.json(question);
   });
-  
-  //Read specific question
-  router.get('/:qID', (req, res) => {
-    res.json({
-      response: 'I have a GET request',
-      body: 'You sent me a specific get request',
-      questionId: req.params.qID,
-      questionId: req.params.qID
-    });
+});
+
+//Read specific question
+router.get('/:qID', (req, res) => {
+  res.json({
+    response: 'I have a GET request',
+    body: 'You sent me a specific get request',
+    questionId: req.params.qID,
+    questionId: req.params.qID,
   });
-  
-  //create answer to specfic qn
-  router.post('/:qID/answers', (req, res) => {
-    res.json({
-      response: 'I have a POST request to answer a question',
-      questionId: req.params.qID,
-      body: req.body
-    });
+});
+
+//create answer to specfic qn
+router.post('/:qID/answers', (req, res) => {
+  res.json({
+    response: 'I have a POST request to answer a question',
+    questionId: req.params.qID,
+    body: req.body,
   });
-  //Read answer to specfic qn
-  router.get('/:qID/answers', (req, res) => {
-    res.json({
-      response: 'I have a GET request to read answers',
-      questionId: req.params.qID,
-      body: req.body
-    });
+});
+//Read answer to specfic qn
+router.get('/:qID/answers', (req, res) => {
+  res.json({
+    response: 'I have a GET request to read answers',
+    questionId: req.params.qID,
+    body: req.body,
   });
-  
-  //Edit specific answer
-  router.put('/:qID/answers/:aID', (req, res) => {
+});
+
+//Edit specific answer
+router.put('/:qID/answers/:aID', (req, res) => {
+  res.json({
+    response: 'I have a PUT request to edit answer',
+    questionId: req.params.qID,
+    answerID: req.params.aID,
+    body: req.body,
+  });
+});
+
+//Delete answer to specfic qn
+router.delete('/:qID/answers/:aID', (req, res) => {
+  res.json({
+    response: 'I have a DELETE request to delete answer',
+    questionId: req.params.qID,
+    answerID: req.params.aID,
+  });
+});
+
+//Vote answer to specfic qn
+router.post(
+  '/:qID/answers/:aID/vote-:dirr',
+  (req, res, next) => {
+    // if (req.params.dirr.search('/^(up|down)$/') === -1) {
+    if (req.params.dirr !== 'up' || req.params.dirr !== 'down') {
+      next();
+    } else {
+      let err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    }
+  },
+  (req, res) => {
     res.json({
-      response: 'I have a PUT request to edit answer',
+      response: 'I have a POST request to vote answer',
       questionId: req.params.qID,
       answerID: req.params.aID,
-      body: req.body
+      voteDirection: req.params.dirr,
+      body: req.body,
     });
-  });
-  
-  //Delete answer to specfic qn
-  router.delete('/:qID/answers/:aID', (req, res) => {
-    res.json({
-      response: 'I have a DELETE request to delete answer',
-      questionId: req.params.qID,
-      answerID: req.params.aID
-    });
-  });
-  
-  //Vote answer to specfic qn
-  router.post(
-    '/:qID/answers/:aID/vote-:dirr',
-    (req, res, next) => {
-      // if (req.params.dirr.search('/^(up|down)$/') === -1) {
-      if (req.params.dirr !== 'up' || req.params.dirr !== 'down') {
-        next();
-      } else {
-        let err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-      }
-    },
-    (req, res) => {
-      res.json({
-        response: 'I have a POST request to vote answer',
-        questionId: req.params.qID,
-        answerID: req.params.aID,
-        voteDirection: req.params.dirr,
-        body: req.body
-      });
-    }
-  );
-  
+  }
+);
 
 //export router
 module.exports = router;
